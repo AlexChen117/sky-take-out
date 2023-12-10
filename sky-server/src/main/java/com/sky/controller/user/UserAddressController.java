dae91fa340e8e76065f0148e7d9d4bd0f7bc1157
Alex <764162908@qq.com>
2023-12-10 21:47:24 +0800

    地址簿相关类创建

---
+++ sky-server/src/main/java/com/sky/controller/user/UserAddressController.java
@@ -0,0 +1,15 @@
+package com.sky.controller.user;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 地址簿
+ */
+@RestController
+@Slf4j
+@RequestMapping("/user/addressBook")
+public class UserAddressController {
+}
